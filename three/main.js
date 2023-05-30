6119470a8d5c750783631ee29807cb193012e058
wangbaishun <863548227@qq.com>
2023-05-30 17:23:10 +0800

    feat:instanceMesh

--- three/main.js
+++ three/main.js
@@ -1,29 +1,32 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // import {OBJLoader}  from 'three/addons/loaders/OBJLoader.js'
-import {TransformControls} from 'three/addons/controls/TransformControls.js'
-import {addShowder,addSnowScene,addBox,loadEarth, addSprite} from './util.js'
+import { TransformControls } from 'three/addons/controls/TransformControls.js'
+import { addShowder, addSnowScene, addBox, loadEarth, addSprite, addGui, addInstanceMesh, ray, addOimo } from './util.js'
 import './reset.css'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-camera.position.set(200, 200, 200)
+camera.position.set(20, 20, 20)
 camera.lookAt(0, 0, 0)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
-const axesHelper = new THREE.AxesHelper( 500 );
-scene.add( axesHelper );
+const axesHelper = new THREE.AxesHelper(500);
+scene.add(axesHelper);
 const transformControls = new TransformControls(camera, renderer.domElement);
 scene.add(transformControls)
 // transformControls.attach(box)
 
 
-addShowder(scene)
+// let sphereMesh=addShowder(scene)
+// addGui(sphereMesh)
+addOimo(scene)
 function render() {
+    
     // earth.rotation.y += 0.01;
     renderer.shadowMap.enabled = true;//开启阴影
     renderer.render(scene, camera)
--- three/util.js
+++ three/util.js
@@ -1,7 +1,10 @@
 import * as THREE from 'three'
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+import { OimoPhysics } from 'three/addons/physics/OimoPhysics.js';
+
 function addBox(scene) {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     return cube;
@@ -105,6 +108,94 @@ function addShowder(scene) {
     sphereMesh.castShadow = true//投射物体产生阴影
     plan.receiveShadow = true//接收阴影
     sportLight.castShadow = true//光源投射阴影
-
+    return sphereMesh
 }
-export { addShowder, addBox, loadEarth, addSprite, addSnowScene, addLight }
+function addGui(mesh) {
+    const gui = new GUI()
+    gui.close()//折叠菜单
+    let poi = gui.addFolder('球体位置')//添加分组
+    poi.add(mesh.position, 'x', -100, 100).name('x位置').step(10).onChange((val) => {
+        console.log(val);
+    })
+    gui.addColor(mesh.material, 'color').name('材料颜色') //添加颜色选择器
+}
+//实例化网格批量创建物体
+function addInstanceMesh(scene) {
+    const geometry = new THREE.IcosahedronGeometry(0.5, 5)
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    let count = 1000
+    let meshes = new THREE.InstancedMesh(geometry, material, count)
+    //循环设置每个实例的位置
+    // 定义每个小球的id索引，作为小球的标识
+    let index = 0
+    let amount = 10
+    // 定义颜色
+    let white = new THREE.Color().setHex(0xffffff)
+    // 定义每个小球位置的偏移量
+    const offset = (amount - 1) / 2 //4.5,即每个小球间隔4.5
+    // 转换矩阵，
+    const matrix = new THREE.Matrix4()
+    for (let i = 0; i < amount; i++) {
+        for (let j = 0; j < amount; j++) {
+            for (let k = 0; k < amount; k++) {
+                // 设置matrix的位置，根据偏移量
+                matrix.setPosition(offset - i, offset - j, offset - k) //-4.5 ~ 4.5
+                // 将index和matrix赋值给meshes
+                meshes.setMatrixAt(index, matrix)
+                let white = new THREE.Color().setHex(Math.random() * 0xffffff)
+
+                // 设置每个小球的颜色
+                meshes.setColorAt(index, white)
+                index = index + 1
+            }
+        }
+    }
+    scene.add(meshes)
+    return meshes
+}
+function ray(mesh) {
+    let lastInstanceId
+    let instancedMesh = addInstanceMesh(scene)
+    const mouse = new THREE.Vector2()
+    const raycaster = new THREE.Raycaster()
+    window.addEventListener('mousemove', function (event) {
+        mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+        raycaster.setFromCamera(mouse, camera)
+
+    })
+    // 通过摄像机和鼠标位置更新射线
+    // console.log(mouse);
+    // 计算物体和射线的焦点
+    const intersection = raycaster.intersectObject(instancedMesh)
+    // console.log(intersection.length)
+    if (intersection.length > 0) {
+        lastInstanceId && instancedMesh.setColorAt(lastInstanceId, new THREE.Color(1, 0, 0))
+        const instanceId = intersection[0].instanceId //获取射线穿透时的第一个物体
+        instancedMesh.setColorAt(instanceId, new THREE.Color(1, 0, 0).setHex(0xffffff))
+        instancedMesh.instanceColor.needsUpdate = true
+        lastInstanceId = instanceId
+    }
+}
+function addPlan(scene) {
+    let planGeometry = new THREE.PlaneGeometry(10, 10,10)
+    let planMaterial = new THREE.MeshBasicMaterial({ color: 0x808080 }) 
+    let plan = new THREE.Mesh(planGeometry, planMaterial)
+    plan.rotation.x = - Math.PI / 2
+    plan.position.set(0, -1, 0)
+    scene.add(plan)
+    return plan
+}
+async function addOimo(scene) {
+    let physics = await OimoPhysics()
+    let box = addBox(scene)
+    box.position.y=10
+    //不传入代表刚体
+    //BUG:刚体会穿透地面
+    let plan = addPlan(scene)
+    console.log('plan: ', plan);
+    //默认为0，即代表小球不参与物理世界的运动，我们把其设置为1
+    physics.addMesh(plan)
+    physics.addMesh(box, 1)
+}
+export { addOimo, addShowder, addInstanceMesh, addGui, ray, addBox, loadEarth, addSprite, addSnowScene, addLight }
